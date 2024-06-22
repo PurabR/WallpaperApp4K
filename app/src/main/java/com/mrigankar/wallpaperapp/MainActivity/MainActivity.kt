210636634edca95a12163b36db61965870443633
Mriganka Bordoloi <mrigankatiwasa@gmail.com>
2024-06-22 23:22:36 +0530

    home

--- app/src/main/java/com/mrigankar/wallpaperapp/MainActivity/MainActivity.kt
+++ app/src/main/java/com/mrigankar/wallpaperapp/MainActivity/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.mrigankar.wallpaperapp.MainActivity
 
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.homedrop.common.base.BaseActivity
+import com.mrigankar.wallpaperapp.R
 import com.mrigankar.wallpaperapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+
+    private lateinit var navController: NavController
     override fun getViewBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
+
     }
 
     override fun setUpViews() {
         super.setUpViews()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            if (it.itemId == R.id.action_home) {
+                navController.navigate(R.id.homeFragment)
+            }
+            if (it.itemId == R.id.action_downloads) {
+                navController.navigate(R.id.action_homeFragment_to_downloadFragment)
+            }
+
+
+
+            return@setOnItemSelectedListener true
+        }
     }
 
 
