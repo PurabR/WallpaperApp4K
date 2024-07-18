3cf01989cdcf77a147ea2134b8853a723ecfba0f
Purab Kashyap <purab2013@gmail.com>
2024-07-18 21:27:15 +0530

    download button added

--- app/src/main/java/com/mrigankar/wallpaperapp/setwallpaper/SetWallpaperFragment.kt
+++ app/src/main/java/com/mrigankar/wallpaperapp/setwallpaper/SetWallpaperFragment.kt
@@ -1,44 +1,32 @@
 package com.mrigankar.wallpaperapp.setwallpaper
 
-import android.annotation.SuppressLint
+import android.app.DownloadManager
 import android.app.WallpaperManager
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
-import android.util.Log
-import android.view.View
-import android.widget.Button
+import android.net.Uri
+import android.os.Environment
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.homedrop.common.base.BaseFragment
 import com.homedrop.common.ktx.showShortToast
 import com.homedrop.common.util.coroutine.launchIO
 import com.homedrop.common.util.image.loadImage
-import com.mrigankar.wallpaperapp.SpecificCategories.SpecificCategoriesFragmentArgs
 import com.mrigankar.wallpaperapp.ViewBinder.ImageBinder.ImageViewData
-import com.mrigankar.wallpaperapp.ViewBinder.categories.CategoriesViewData
 import com.mrigankar.wallpaperapp.ViewBinder.setWallpaper.SetWallpaperViewData
-import com.mrigankar.wallpaperapp.ViewBinder.specificCategories.SpecificCategoriesViewData
-import com.mrigankar.wallpaperapp.adapter.ImageAdapter
-import com.mrigankar.wallpaperapp.adapter.ImageAdapterListener
 import com.mrigankar.wallpaperapp.adapter.SetWallpaperAdapter
 import com.mrigankar.wallpaperapp.adapter.SetWallpaperAdapterListener
-import com.mrigankar.wallpaperapp.adapter.SpecificCategoriesAdapterListener
 import com.mrigankar.wallpaperapp.databinding.FragmentSetWallpaperBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import java.io.File
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class SetWallpaperFragment : BaseFragment<FragmentSetWallpaperBinding, SetWallpaperViewModel>(),
@@ -84,6 +72,8 @@ class SetWallpaperFragment : BaseFragment<FragmentSetWallpaperBinding, SetWallpa
                             val wallpaperManger =
                                 requireContext().getSystemService(Context.WALLPAPER_SERVICE) as WallpaperManager
                             wallpaperManger.setBitmap(resource)
+
+                            showShortToast(requireContext(), "Wallpaper Successfully set")
                         }
 
                         override fun onLoadCleared(placeholder: Drawable?) {
@@ -91,8 +81,47 @@ class SetWallpaperFragment : BaseFragment<FragmentSetWallpaperBinding, SetWallpa
                     })
             }
 
+        }
+
+        binding.downloadImage.setOnClickListener {
+            viewLifecycleOwner.lifecycleScope.launchIO {
+                Glide.with(requireContext()).asBitmap().load(extras.link)
+                    .into(object : CustomTarget<Bitmap>() {
+
+                        override fun onResourceReady(
+                            resource: Bitmap,
+                            transition: Transition<in Bitmap>?
+                        ) {
+                            try {
+                                val dm = requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                                val downloaduri = Uri.parse(extras.link)
+                                val request = DownloadManager.Request(downloaduri)
+                                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                                    .setAllowedOverRoaming(false)
+                                    .setTitle("downloaded image")
+                                    .setMimeType("image/jpeg") // Your file type. You can use this code to download other file types also.
+                                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                                    .setDestinationInExternalPublicDir(
+                                        Environment.DIRECTORY_PICTURES,
+                                        File.separator + "downloaded image" + ".jpg"
+                                    )
+                                dm.enqueue(request)
+                                showShortToast(requireContext(), "Image downloading started")
+                            }catch (e: Exception){
+                                showShortToast(requireContext(), "Image downloading failed")
+                            }
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                        }
+                    })
+            }
 
         }
+
+
+
+
 
 
 
@@ -123,9 +152,12 @@ class SetWallpaperFragment : BaseFragment<FragmentSetWallpaperBinding, SetWallpa
 
     }
 
+
     override fun onSetWallpaperClicked(setWallpaper: SetWallpaperViewData) {
         showShortToast(requireContext(), "Image clicked")
     }
+
+
 
 
 }
